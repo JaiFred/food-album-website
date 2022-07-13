d15a25ffd24164167e1945db467342ca332667bb
JaiVon <frederickjaivon@yahoo.com>
2022-07-13 16:03:31 -0400

    Updated components

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -5,7 +5,18 @@ function FoodCard ({ food }) {
     return(
         <li className="card-data">
             <div className="card">
-                <img src={food.image}></img>
+                <img 
+                src={food.image} 
+                alt={food.name} 
+                className="card-image" 
+                />
+                <div className="card-content">
+                    <div className="card_title">{food.name}</div>
+                    <p className="card_text">{food.about}</p>
+                    <div className="card_detail">
+                        <p>{food.description.type}</p>
+                    </div>
+                </div>
             </div>
         </li>
     )
